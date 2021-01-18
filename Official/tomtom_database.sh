9e2c528d07b06a7a6bd9c601f0b65b79487fe329
Rohan75 <ghotrarohan@gmail.com>
2021-01-18 09:39:20 -0500

    a

--- Official/tomtom_database.sh
+++ Official/tomtom_database.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-direc='./motifs'
+model_name=$1
+
+direc="./motifs"
+
+mkdir -p -- "./results/${model_name}"
 
 for motif_num in {1..32}
 do
-    tomtom -evalue -thresh 0.1 -o "./results/filter-${motif_num}" "$direc/model-test/filter-${motif_num}.txt" "$direc/motif_database.meme"
+    tomtom -evalue -thresh 0.1 -o "./results/${model_name}/filter-${motif_num}" "$direc/model-test/filter-${motif_num}.txt" "$direc/motif_database.meme"
 done
