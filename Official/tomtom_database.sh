6c5c7f7341fcfa7e179c9e37761d650196038046
Rohan75 <ghotrarohan@gmail.com>
2021-01-18 15:21:19 -0500

    changed tomtom target directory

--- Official/tomtom_database.sh
+++ Official/tomtom_database.sh
@@ -8,5 +8,5 @@ mkdir -p -- "./results/${model_name}"
 
 for motif_num in {1..32}
 do
-    tomtom -evalue -thresh 0.1 -o "./results/${model_name}/filter-${motif_num}" "$direc/model-test/filter-${motif_num}.txt" "$direc/motif_database.meme"
+    tomtom -evalue -thresh 0.1 -o "./results/${model_name}/filter-${motif_num}" "$direc/${model_name}/filter-${motif_num}.txt" "$direc/motif_database.meme"
 done
