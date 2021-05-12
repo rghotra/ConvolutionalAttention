d04f81699d5a4b19de7da6f7021d5f7658c34a77
Rohan75 <ghotrarohan@gmail.com>
2021-05-12 12:09:01 -0400

    update

---
+++ Task1/models.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras import layers, Model, Input
+
+from tfomics import moana, evaluate
+from tfomics.layers import MultiHeadAttention
+
+def CNN_ATT(in_shape=(200, 4), num_filters=32, batch_norm=True, activation='relu', pool_size=4, heads=8, key_size=64, dense_units=512):
+
+    inputs = Input(shape=in_shape)
+    nn = layers.Conv1D(filters=num_filters, kernel_size=19, use_bias=False, padding='same')(inputs)
+    if batch_norm:
+        nn = layers.BatchNormalization()(nn)
+    nn = layers.Activation(activation, name='conv_activation')(nn)
+    nn = layers.MaxPool1D(pool_size=pool_size)(nn)
+    nn = layers.Dropout(0.1)(nn)
+
+    nn, w = MultiHeadAttention(num_heads=heads, d_model=key_size)(nn, nn, nn)
+    nn = layers.Dropout(0.1)(nn)
+
+    nn = layers.Flatten()(nn)
+
+    nn = layers.Dense(dense_units, use_bias=False)(nn)
+    nn = layers.BatchNormalization()(nn)
+    nn = layers.Activation('relu')(nn)
+    nn = layers.Dropout(0.5)(nn)
+
+    outputs = layers.Dense(12, activation='sigmoid')(nn)
+
+    return Model(inputs=inputs, outputs=outputs)
--- Task1/utils.py
+++ Task1/utils.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras import layers, Model
+from tfomics import moana
 
 import numpy as np
 import requests as rq
 import os, io, h5py
+
+import subprocess
+import shlex
 
 import models
 
@@ -24,15 +28,13 @@ def get_synthetic_dataset():
 
     return x_train, y_train, x_valid, y_valid, x_test, y_test
 
-def train_model(model, train, valid, epochs=75, seed=None):
-    if seed is not None:
-        tf.random.set_seed(seed)
+def get_ppms(model, x_test):
+    index = [type(i) for i in model.layers].index(tf.keras.layers.Activation)
     
-    auroc = tf.keras.metrics.AUC(curve='ROC', name='auroc')
-    aupr = tf.keras.metrics.AUC(curve='PR', name='aupr')
-    model.compile(tf.keras.optimizers.Adam(0.0005), loss='binary_crossentropy', metrics=[auroc, aupr])
+    ppms = moana.filter_activations(x_test, model, layer=index, window=20,threshold=0.5)
+    ppms = moana.clip_filters(ppms, threshold=0.5, pad=3)
+    
+    return ppms
 
-    lr_decay = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_aupr', factor=0.2, patient=5, verbose=1, min_lr=1e-7, mode='max')
-    model.fit(train[0], train[1], epochs=epochs, validation_data=(valid[0], valid[1]), callbacks=[lr_decay], verbose=1)
-    
-    
+def tomtom(motif_dir, output_dir):
+    subprocess.call(shlex.split(f'./motif_comparison.sh {motif_dir} {output_dir}'))
