2ac159200fbc24b85de0669b0964b97fd3174349
Rohan75 <ghotrarohan@gmail.com>
2021-05-12 20:13:10 -0400

    finished pipeline

--- Task1/utils.py
+++ Task1/utils.py
@@ -1,17 +1,11 @@
-import tensorflow as tf
-from tensorflow.keras import layers, Model
-from tfomics import moana
+from tfomics import moana, evaluate
 
 import numpy as np
 import requests as rq
-import os, io, h5py
+import io, h5py
 
 import subprocess
 import shlex
-
-import models
-
-tf.config.run_functions_eagerly(True)
 
 def get_synthetic_dataset():
 
@@ -29,7 +23,7 @@ def get_synthetic_dataset():
     return x_train, y_train, x_valid, y_valid, x_test, y_test
 
 def get_ppms(model, x_test):
-    index = [type(i) for i in model.layers].index(tf.keras.layers.Activation)
+    index = [i.name for i in model.layers].index('conv_activation')
     
     ppms = moana.filter_activations(x_test, model, layer=index, window=20,threshold=0.5)
     ppms = moana.clip_filters(ppms, threshold=0.5, pad=3)
@@ -37,4 +31,21 @@ def get_ppms(model, x_test):
     return ppms
 
 def tomtom(motif_dir, output_dir):
+    subprocess.call(shlex.split('chmod +x ./motif_comparison.sh'))
     subprocess.call(shlex.split(f'./motif_comparison.sh {motif_dir} {output_dir}'))
+
+def analysis(name, motif_dir, tomtom_dir, model, x_test, y_test):
+    model_name = name
+
+    num_filters = moana.count_meme_entries(motif_dir)
+    model_match_frac, match_any, filter_matches, filter_qvalues, motif_qvalues, hit_counts = evaluate.motif_comparison_synthetic_dataset(tomtom_dir + '/tomtom.tsv', num_filters=num_filters)
+    model_false_frac = match_any - model_match_frac
+
+    results = model.evaluate(x_test, y_test, verbose=2)
+    model_loss = results[0]
+    model_auroc = results[1]
+    model_aupr = results[2]
+
+    statistics = np.array([model_name, model_loss, model_auroc, model_aupr, model_match_frac, model_false_frac])
+    return statistics
+    
